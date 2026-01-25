0e0e8a7f791a1f4e8d5e13c61ff1a1892a831c77
Akshay Jain <akshay.j.5335.com>
2026-01-25 21:41:19 +0530

    Clean up: Remove unused files

--- test_event_model.js
+++
@@ -1,5 +0,0 @@
-import mongoose from 'mongoose';
-import Event from './backend/models/events.js';
-
-console.log('Event model imported successfully:', Event);
-console.log('Schema paths:', Object.keys(Event.schema.paths));
